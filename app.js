1e8392229e47408e943f8871126b7873fb91f4b1
Mykhailo Golota <mykhailo.golota@panenco.com>
2021-03-01 22:46:33 +0200

    feat: currency

--- app.js
+++ app.js
@@ -14,11 +14,54 @@ if (process.env.NODE_ENV === 'production') {
    bot = new TelegramBot(token, { polling: true });
 }
 
-bot.on('message', (msg) => {
- if(msg.text === 'Cho cho'){
-     bot.sendMessage(chatId, "opa nihua")
- }
+bot.onText(/\/curse/, (msg, match) => {
+
+  const chatId = msg.chat.id;
+
+  bot.sendMessage(chatId, 'Выберите какая валюта вас интересует', {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          {
+            text: '€ - EUR',
+            callback_data: 'EUR'
+          }, {
+            text: '$ - USD',
+            callback_data: 'USD'
+          }, {
+            text: '₽ - RUR',
+            callback_data: 'RUR'
+          }, {
+            text: '₿ - BTC',
+            callback_data: 'BTC'
+          }
+        ]
+      ]
+    }
+  });
 });
+
+bot.on('callback_query', query => {
+  const id = query.message.chat.id;
+
+  request('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5', function (error, response, body) {
+    const data = JSON.parse(body);
+    const result = data.filter(item => item.ccy === query.data)[0];
+    const flag = {
+      'EUR': '🇪🇺',
+      'USD': '🇺🇸',
+      'RUR': '🇷🇺',
+      'UAH': '🇺🇦',
+      'BTC': '₿'
+    }
+    let md = `
+      *${flag[result.ccy]} ${result.ccy} 💱 ${result.base_ccy} ${flag[result.base_ccy]}*
+      Buy: _${result.buy}_
+      Sale: _${result.sale}_
+    `;
+    bot.sendMessage(id, md, {parse_mode: 'Markdown'});
+  })
+})
 const app = express();
 
 app.use(bodyParser.json());
