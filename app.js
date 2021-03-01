33b1f6fb2faf31202a39b77f2b9a6c21f76fbe7c
Mykhailo Golota <mykhailo.golota@panenco.com>
2021-03-01 22:25:39 +0200

    feat: deploy changes

---
+++ app.js
@@ -0,0 +1,32 @@
+const TelegramBot = require('node-telegram-bot-api');
+const axios = require('axios');
+const express = require('express')
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+const token = process.env.TELEGRAM_TOKEN;
+let bot;
+
+if (process.env.NODE_ENV === 'production') {
+   bot = new TelegramBot(token);
+   bot.setWebHook(process.env.HEROKU_URL + bot.token);
+} else {
+   bot = new TelegramBot(token, { polling: true });
+}
+
+// Matches "/word whatever"
+bot.onText(/\/word (.+)/, (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, "Something")
+});
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.listen(process.env.PORT);
+
+app.post('/' + bot.token, (req, res) => {
+  bot.processUpdate(req.body);
+  res.sendStatus(200);
+});
